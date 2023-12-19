025423596346a362b961336f5e2ed9addbf420a8
Natalie Clark <nj_clark@hotmail.co.uk>
2023-12-19 19:48:33 +0000

    Add comments to improve readability

--- lib/change_debugging.py
+++ lib/change_debugging.py
@@ -1,2 +1,12 @@
+# EXERCISE: Use change debugging to fix this program ...
+# def say_hello(name):
+#     return "hello {name}"
+
+# Intended output:
+#
+# > say_hello("kay")
+# => "hello kay"
+
+#  SOLUTION:
 def say_hello(name):
     return f"hello {name}"
--- lib/getting_visibility.py
+++ lib/getting_visibility.py
@@ -1,3 +1,22 @@
+#  EXERCISE: Get visability to fix this program ...
+# def factorial(n):
+#     product = 1
+#     print(f"at the start product is {product}")
+#     while n > 0:
+#         n -= 1
+#         print(f"we multiply {product} by {n}")
+#         product *= n
+#         print(f"we get {product}")
+      
+#     return product
+
+# print(f"""
+#  Running: factorial(5)
+# Expected: 120
+#   Actual: {factorial(5)}
+# """)
+
+#  SOLUTION:
 def factorial(n):
     product = 1
     # print(f"at the start product is {product}")
